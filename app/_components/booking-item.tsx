181cf438a9575930cd0e6c86f6f76b18a1ddc3b4
Victor Emanoel <victoremaanoel@outlook.com>
2024-08-06 11:08:53 -0300

    refactor: Componentizing bookingItem

---
+++ app/_components/booking-item.tsx
@@ -0,0 +1,41 @@
+import { Avatar } from "./ui/avatar"
+import { AvatarImage } from "@radix-ui/react-avatar"
+import { Badge } from "./ui/badge"
+import { Card, CardContent } from "./ui/card"
+
+//TODO recive bookingItems with props
+
+const BookingItem = () => {
+  return (
+    <>
+      {" "}
+      {/* Como precisa renderizar varios elementos usa-se fragment <></> */}
+      <div className="mt-5">
+        <h4 className="uppercase text-gray-400">Agendamentos</h4>
+      </div>
+      <Card className="mt-2">
+        <CardContent className="flex justify-between p-0">
+          {/* Esquerda */}
+          <div className="flex flex-col gap-2 py-5 pl-5">
+            <Badge className="w-fit">Confirmado</Badge>
+            <h3 className="font-semibold">Corte de Cabelo</h3>
+            <div className="flex items-center gap-2">
+              <Avatar className="h-10 w-10">
+                <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+              </Avatar>
+              <p className="text-sm">Barbearia Test</p>
+            </div>
+          </div>
+          {/* Direita */}
+          <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+            <p className="text-sm">Agosto</p>
+            <p className="text-2xl">05</p>
+            <p className="text-sm">22:00</p>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  )
+}
+
+export default BookingItem
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,11 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
-import { Badge } from "./_components/ui/badge"
 import { Card, CardContent } from "./_components/ui/card"
-import { Avatar } from "./_components/ui/avatar"
-import { AvatarImage } from "@radix-ui/react-avatar"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
 import { quickSearchOption } from "./_constants/quickSearchOptions"
+import BookingItem from "./_components/booking-item"
 
 export default async function Home() {
   const barbershop = await db.barbershop.findMany({})
@@ -22,6 +20,7 @@ export default async function Home() {
         {/* Nome Usuario e dia */}
         <h2 className="text-xl font-bold">Ola, Victor!</h2>
         <p>Segunda Feira, 05 Agosto.</p>
+
         {/* Input Pesquisa */}
         <div className="mt-4 flex items-center gap-2">
           <Input placeholder="Pesquise Aqui." />
@@ -29,6 +28,7 @@ export default async function Home() {
             <SearchIcon />
           </Button>
         </div>
+
         {/* Busca Rapida */}
         <div className="mt-5 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
           {quickSearchOption.map((option) => (
@@ -43,6 +43,7 @@ export default async function Home() {
             </Button>
           ))}
         </div>
+
         {/* Banner */}
         <div className="relative mt-5 h-[150px] w-full">
           <Image
@@ -52,38 +53,20 @@ export default async function Home() {
             alt="Agendar"
           />
         </div>
+
         {/* Agendamentos */}
-        <div className="mt-5">
-          <h4 className="uppercase text-gray-400">Agendamentos</h4>
-        </div>
-        <Card className="mt-2">
-          <CardContent className="flex justify-between p-0">
-            {/* Esquerda */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-10 w-10">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia Test</p>
-              </div>
-            </div>
-            {/* Direita */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">22:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
+
+        {/* Barbearias */}
         <h4 className="mt-5 uppercase text-gray-400">Recomendados</h4>
         <div className="mt-2 flex gap-4 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
           {barbershop.map((barbershop) => (
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
-      </div>
+      </div>{" "}
+      {/* Fim conteudo geral */}
+      {/* footer */}
       <footer>
         <Card>
           <CardContent className="px-5 py-6 text-center">

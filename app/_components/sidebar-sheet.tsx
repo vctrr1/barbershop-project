5d8a83ee89aef7f7f502088ac2411cea1e89f129
Victor Emanoel <victoremaanoel@outlook.com>
2024-08-08 17:54:42 -0300

    fix:  wrap text name and email on sidebar

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -17,8 +17,12 @@ const SidebarSheet = () => {
               <AvatarImage src="https://plus.unsplash.com/premium_photo-1678197937465-bdbc4ed95815?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
             </Avatar>
             <div className="flex flex-col items-start">
-              <p className="font-bold">Nome do Usuário</p>
-              <p className="text-xs text-gray-300">google@gmail.com</p>
+              <p className="max-w-[150px] truncate font-bold">
+                Victor Emanoel M Cabral
+              </p>
+              <p className="max-w-[150px] truncate text-xs text-gray-300">
+                victtoremmanoel@outlook.com
+              </p>
             </div>
           </div>
           <Button variant="outline">

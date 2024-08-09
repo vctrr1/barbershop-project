72a01e3cc3682be699c4f9dbd7391ded5f96914a
Victor Emanoel <victoremaanoel@outlook.com>
2024-08-09 12:05:35 -0300

    feat: add Link to home page in logo on header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,12 +4,15 @@ import { Button } from "./ui/button"
 import { MenuIcon } from "lucide-react"
 import { Sheet, SheetTrigger } from "./ui/sheet"
 import SidebarSheet from "./sidebar-sheet"
+import Link from "next/link"
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="flex flex-row items-center justify-between p-2 pl-3 pr-3">
-        <Image alt="Barber" src="/logo.png" height={15} width={100} />
+        <Link href="/">
+          <Image alt="Barber" src="/logo.png" height={15} width={100} />
+        </Link>
         <Sheet>
           <SheetTrigger asChild>
             <Button size="icon" variant="outline">

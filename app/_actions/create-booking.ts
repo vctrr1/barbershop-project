1504faa736af4eef6796bf488b8771d416367b68
Victor Emanoel <victoremaanoel@outlook.com>
2024-08-12 01:06:19 -0300

    fix: ensure that the unauthenticated use has acess function create bookin via http

--- app/_actions/create-booking.ts
+++ app/_actions/create-booking.ts
@@ -2,18 +2,33 @@
 
 import { revalidatePath } from "next/cache"
 import { db } from "../_lib/prisma"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../_lib/auth"
 
 interface CreateBookingProps {
   serviceId: string
   userId: string
   date: Date
 }
+//poderia ser passado os parametros desse jeto, ja que vai pegar todos que passar
+//const CreateBooking = async (params: CreateBookingProps) => {
+//  await db.booking.create({
+//    data: params,
+//  })
+//  revalidatePath("/barbershop/[id]")
+//}
 
 const CreateBooking = async ({
   serviceId,
   userId,
   date,
 }: CreateBookingProps) => {
+  const user = await getServerSession(authOptions)
+
+  if (!user) {
+    throw new Error("Usuário não autenticado")
+  }
+
   await db.booking.create({
     data: {
       serviceId,
